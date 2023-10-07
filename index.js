02611eff21edef60f8e9b02456e15a93a8d9192a
gkaparos <gkaparos@hotmail.com>
2023-10-07 15:17:31 +0300

    jest tests and dockerfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+
 
 const cors = require('cors');
 const user = require('./routes/user.route');
@@ -39,6 +39,5 @@ swaggerUI.serve,
 swaggerUI.setup(swaggerDocument.options)
 );
 
-app.listen(port, () => {
-    console.log("Listening on port 3000");
-})
+module.exports = app;
+
